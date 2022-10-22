71e0a03f19261430d73b2bb20c254d81116d3e94
shar3nda <vyavya2003@yandex.ru>
2022-10-23 00:41:47 +0300

    add comments to printArray

--- asm/printArray.s
+++ asm/printArray.s
@@ -3,7 +3,7 @@
 	.text
 	.section	.rodata
 .LC0:
-	.string	"%i "
+	.string	"%i "  # "%i " для printf
 	.text
 	.globl	printArray
 	.type	printArray, @function
@@ -16,27 +16,27 @@ printArray:
 	mov	rbp, rsp
 	.cfi_def_cfa_register 6
 	sub	rsp, 32
-	mov	QWORD PTR -24[rbp], rdi
-	mov	DWORD PTR -28[rbp], esi
-	mov	DWORD PTR -4[rbp], 0
+	mov	QWORD PTR -24[rbp], rdi  # int *b
+	mov	DWORD PTR -28[rbp], esi  # int length
+	mov	DWORD PTR -4[rbp], 0  # i = 0
 	jmp	.L2
 .L3:
 	mov	eax, DWORD PTR -4[rbp]
 	cdqe
 	lea	rdx, 0[0+rax*4]
 	mov	rax, QWORD PTR -24[rbp]
-	add	rax, rdx
+	add	rax, rdx  # rax = b[i]
 	mov	eax, DWORD PTR [rax]
 	mov	esi, eax
-	lea	rax, .LC0[rip]
+	lea	rax, .LC0[rip]  # rax = "%i "
 	mov	rdi, rax
 	mov	eax, 0
-	call	printf@PLT
-	add	DWORD PTR -4[rbp], 1
+	call	printf@PLT  # printf("%i ", b[i])
+	add	DWORD PTR -4[rbp], 1  # ++i
 .L2:
 	mov	eax, DWORD PTR -4[rbp]
 	cmp	eax, DWORD PTR -28[rbp]
-	jl	.L3
+	jl	.L3  # if i < length continue
 	nop
 	nop
 	leave
